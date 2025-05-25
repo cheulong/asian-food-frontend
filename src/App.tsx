e72f38a58cfe815aa05d5f4268c08c4ce1c7cf95
Cheulong Sear <searcheulong@gmail.com>
2025-05-25 20:28:00 +0700

    dockerize asian food frontend

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+type Menu = {
+  id: number
+  name: string
+  price: number
+}
+function App() {
+  const [menus, setMenus] = useState<Menu[]>([])
+const version = APP_VERSION
+  useEffect(() => {
+    fetch('http://localhost:3000/v1/menus')
+      .then((res) => res.json())
+      .then((data) => {
+        setMenus(data)
+      })
+  }, [])
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      <p className='label'>v{version}</p>
+      <h1 className='title-text'>Asian Food Menu<br/><span className='jp-menu'>メニュー</span></h1>
+      <div  className='title'/>
+      {menus.length > 0 ? (
+        <ul>
+          {menus.map((menu) => (
+            <li key={menu.id} className="menu-item">
+                <p>{menu.name}</p>
+                <p>{menu.price}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading menus...</p>
+      )}
+    </>
+  )
+}
+
+export default App
